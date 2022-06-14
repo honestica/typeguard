b975b4adb11e7ba07dd09f3e909b12feefaa3641
Zamralik <vuk.kulvar+github@gmail.com>
2022-06-14 12:47:23 +0200

    Configuration change

--- .markdownlint-cli2.cjs
+++ .markdownlint-cli2.cjs
@@ -1,15 +1,16 @@
-const { printWidth } = require("./prettier.config.cjs");
-
 module.exports = {
-  ignores: ["CHANGELOG.md", "node_modules"],
-  config: {
-    default: true,
-    line_length: {
-      line_length: printWidth,
-    },
-    "no-inline-html": {
-      allowed_elements: ["div", "br"],
-    },
-    "no-duplicate-header": false,
-  },
+	ignores: ["CHANGELOG.md", "node_modules"],
+	config: {
+		default: true,
+		line_length: {
+			line_length: 120,
+		},
+		"no-inline-html": {
+			allowed_elements: ["div", "br"],
+		},
+		"no-duplicate-header": false,
+		MD010: {
+			code_blocks: false
+		}
+	},
 };
