d8426ced07ac7ecd3a5ea100cca972990db07a11
Zamralik <vuk.kulvar+github@gmail.com>
2022-06-14 12:42:59 +0200

    Configuration change

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,7 +1,5 @@
 module.exports = {
-  "package.json": ["yarn packagejson:format", "yarn packagejson:lint"],
-  "**/*.ts": () => ["tsc -p tsconfig.json --noEmit"],
-  "**/*.{ts,mts,cts,js,mjs,cjs}": (filenames) => `eslint ${filenames.join(" ")} --fix --cache`,
-  "*": (filenames) => `prettier --ignore-unknown --write ${filenames.join(" ")}`,
-  "**/*.md": (filenames) => `markdownlint-cli2-fix ${filenames.join(" ")}`,
+	"package.json": ["yarn packagejson:format", "yarn packagejson:lint"],
+	"**/*.ts": () => ["tsc -p tsconfig.json --noEmit"],
+	"**/*.{ts,mts,cts,js,mjs,cjs}": (filenames) => `eslint -c .eslintrc.cjs ${filenames.join(" ")} --fix --cache`,
 };
