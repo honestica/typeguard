177520fd1939acb56a5ee23d264de666e1769012
Zamralik <vuk.kulvar+github@gmail.com>
2022-06-14 12:47:23 +0200

    Configuration change

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,5 +1,4 @@
 module.exports = {
-	"package.json": ["yarn packagejson:format", "yarn packagejson:lint"],
 	"**/*.ts": () => ["tsc -p tsconfig.json --noEmit"],
 	"**/*.{ts,mts,cts,js,mjs,cjs}": (filenames) => `eslint -c .eslintrc.cjs ${filenames.join(" ")} --fix --cache`,
 };
