c840ef7b68d5d5c19af55bfaae3008db65b2c66f
Zamralik <vuk.kulvar+github@gmail.com>
2022-06-14 12:42:59 +0200

    Configuration change

--- prettier-package-json.config.cjs
+++ prettier-package-json.config.cjs
@@ -1,79 +1,77 @@
-const { useTabs, tabWidth } = require("./prettier.config.cjs");
-
 module.exports = {
-  useTabs: useTabs,
-  tabWidth: tabWidth,
-  expandUsers: true,
-  keyOrder: [
-    /**
-     * Details
-     */
-    "$schema",
-    "name",
-    "version",
-    "description",
-    "author",
-    "maintainers",
-    "contributors",
-    "homepage",
-    "bugs",
+	useTabs: true,
+	tabWidth: 4,
+	expandUsers: true,
+	keyOrder: [
+		/**
+		* Details
+		*/
+		"$schema",
+		"name",
+		"version",
+		"description",
+		"author",
+		"maintainers",
+		"contributors",
+		"homepage",
+		"bugs",
 
-    /**
-     * Package publishing configuration
-     */
-    "license",
-    "private",
-    "repository",
-    "publishConfig",
-    "type",
+		/**
+		* Package publishing configuration
+		*/
+		"license",
+		"private",
+		"repository",
+		"publishConfig",
+		"type",
 
-    /**
-     * Configuration
-     */
-    "main",
-    "module",
-    "bin",
-    "browser",
-    "exports",
-    "files",
-    "types",
-    "man",
-    "preferGlobal",
-    "directories",
-    "scripts",
-    "config",
-    "sideEffects",
-    "typings",
+		/**
+		* Configuration
+		*/
+		"main",
+		"module",
+		"bin",
+		"browser",
+		"exports",
+		"files",
+		"types",
+		"man",
+		"preferGlobal",
+		"directories",
+		"scripts",
+		"config",
+		"sideEffects",
+		"typings",
 
-    /**
-     * Dependencies
-     */
-    "dependencies",
-    "bundleDependencies",
-    "bundledDependencies",
-    "peerDependencies",
-    "peerDependenciesMeta",
-    "optionalDependencies",
-    "devDependencies",
+		/**
+		* Dependencies
+		*/
+		"dependencies",
+		"bundleDependencies",
+		"bundledDependencies",
+		"peerDependencies",
+		"peerDependenciesMeta",
+		"optionalDependencies",
+		"devDependencies",
 
-    /**
-     * Yarn specific
-     */
-    "workspaces",
-    "resolutions",
+		/**
+		* Yarn specific
+		*/
+		"workspaces",
+		"resolutions",
 
-    /**
-     * Constraints
-     */
-    "packageManager",
-    "engines",
-    "engineStrict",
-    "os",
-    "cpu",
+		/**
+		* Constraints
+		*/
+		"packageManager",
+		"engines",
+		"engineStrict",
+		"os",
+		"cpu",
 
-    /**
-     * Used for npm search
-     */
-    "keywords",
-  ],
+		/**
+		* Used for npm search
+		*/
+		"keywords",
+	],
 };
